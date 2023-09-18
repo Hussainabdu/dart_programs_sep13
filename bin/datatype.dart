78ebb5c246dfb30f8c70ecc213d831bf3265efe2
Hussainabdu <hussainbold12>
2023-09-18 13:09:00 +0530

    Initial commit

---
+++ bin/datatype.dart
@@ -0,0 +1,13 @@
+void main(){
+  String name = 'Hussain';
+  int age = 20;
+  int phone = 9567430170;
+  double cgpa = 8.1;
+  String email = 'hussainbold12@gmail.com';
+  print('My name is $name');
+  print('I am $age yrs old');
+  print('My phone number is $phone');
+  print('$cgpa is may CGPA in graduation');
+  print('My email is $email');
+
+}
---
+++ test/dart_programs_sep13_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_programs_sep13/dart_programs_sep13.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
